2a4ce061a121841e47ae6e3c5a17dd1399de0d2d
g.rokah@yandex.ru <g.rokah@yandex.ru>
2024-03-25 21:25:43 +0300

    additional commit

---
+++ src/main/java/org/example/pages/BasePage.java
@@ -0,0 +1,46 @@
+package org.example.pages;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public abstract class BasePage {
+    private static final int WAIT_FOR_ELEMENT_TIMEOUT_SECONDS = 100;
+    private static final int DEFAULT_TIME_OF_SECONDS = 1;
+
+    private static final ThreadLocal<WebDriver> driverThreadLocal = new ThreadLocal<>();
+
+    public static WebDriver getDriver() {
+        return driverThreadLocal.get();
+    }
+
+    public BasePage(WebDriver driver) {
+        driverThreadLocal.set(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    protected void waitForAllElementsTimeoutSeconds(List<WebElement> elements) {
+        new WebDriverWait(getDriver(), Duration.ofSeconds(WAIT_FOR_ELEMENT_TIMEOUT_SECONDS))
+                .until(ExpectedConditions.visibilityOfAllElements(elements));
+    }
+
+    protected void waitForElementEnable(WebElement element) {
+        new WebDriverWait(getDriver(), Duration.ofSeconds(WAIT_FOR_ELEMENT_TIMEOUT_SECONDS))
+                .until(ExpectedConditions.visibilityOf(element));
+    }
+
+    protected void timeSleep() {
+        try {
+            TimeUnit.SECONDS.sleep(DEFAULT_TIME_OF_SECONDS);
+        } catch (InterruptedException e) {
+            System.err.print(e.getMessage());
+        }
+    }
+}
